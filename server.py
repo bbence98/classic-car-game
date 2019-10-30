f500e693c5ce63c11d037a6a1b30e99af9626943
davidMolnar012 <davmmm@gmail.com>
2019-10-30 10:21:52 +0100

    Resolve merge conflict

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return render_template('index.html', row_num=7, col_num=7, win_size=3)
+    return render_template('index.html', row_num=28, col_num=14, win_size=3)
 
 if __name__ == '__main__':
     app.run()
--- static/js/game.js
+++ static/js/game.js
@@ -38,16 +38,12 @@ function showHide(firstElement, secondElement) {
 
 
 function main() {
-
     const menu = document.querySelector('#main-menu');
     const gameBoard = document.querySelector('.game-board');
     gameBoard.style.display = 'none';
     const img = document.querySelector('#main-menu img');
     menu.addEventListener('click', function () {
         showHide(img, gameBoard)
-
-
-
     });
 
 
